6af94ebb563eaa5e1a750432de5bdbc2ebe7cf34
Drue Johnson <drueby@gmail.com>
2018-07-25 17:32:38 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,41 @@
-# Write your cod here
+# Challenge 1
+def usd_to_eur(money)
+  puts money*0.86
+end
+usd_to_eur(2)
+
+def eur_to_usd(money)
+  puts money *1.2
+end
+eur_to_usd(2)
+
+def usd_to_yen(money)
+  puts money *110.98
+end
+usd_to_yen(2)
+
+def usd_to_gbp(money)
+    puts money*0.76
+  end
+usd_to_gbp(2)
+  
+  def usd_to_aud(money)
+    puts money*1.35
+  end
+  usd_to_aud(2)
+  
+   def yen_to_aud(money)
+    puts money*0.012
+  end
+  yen_to_aud(2)
+  
+   def gbp_to_usd(money)
+    puts money*1.32
+  end
+  gbp_to_usd(2)
+  
+   def aud_to_usd(money)
+    puts money*0.74
+  end
+  aud_to_usd(2)
+  
